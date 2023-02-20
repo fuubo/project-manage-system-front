8fbf684df5367575431b750683dc440c4503504c
付博1 <fubo1@xdf.cn>
2023-02-20 10:45:48 +0800

    theme

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import './assets/main.css'
 import App from './App.vue'
 import router from './router'
-import './assets/main.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'virtual:svg-icons-register'; //这一句必须要加上
 import SvgIcon from './components/icons/SvgIcon.vue';
-
 const app = createApp(App)
 
 app.component('SvgIcon', SvgIcon)

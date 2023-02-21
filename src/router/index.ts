c4fd8cfa76900b86e31a1d281e7d3b51bdd86e0f
付博1 <fubo1@xdf.cn>
2023-02-21 11:27:05 +0800

    去掉keepalive

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,32 +9,28 @@ const router = createRouter({
       path: '/feature/list',
       name: 'featureList',
       meta: {
-        title: '需求列表',
-        keepAlive: true
+        title: '需求列表'
       },
       component: () => import('../views/feature/FeatureList.vue')
     }, {
       path: '/feature/taskList',
       name: 'featureTaskList',
       meta: {
-        title: '任务列表',
-        keepAlive: true
+        title: '任务列表'
       },
       component: () => import('../views/feature/FeatureTaskList.vue')
     }, {
       path: '/feature/chart',
       name: 'featureChart',
       meta: {
-        title: '需求总览',
-        keepAlive: true
+        title: '需求总览'
       },
       component: () => import('../views/feature/FeatureChart.vue')
     }, {
       path: '/system',
       name: 'system',
       meta: {
-        title: '系统设置',
-        keepAlive: true
+        title: '系统设置'
       },
       component: () => import('../views/base/SystemSetting.vue')
     }]
